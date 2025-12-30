8bb67b77d0faa0b2766007ff45a2711517178a6c
wuxuanok <2031610355@qq.com>
2025-12-30 19:42:01 +0800

    用户上线功能

--- server.go
+++ server.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip string
 	Port int
+
+	//在线用户的列表
+	OnlineMap map[string]*User
+	mapLock sync.RWMutex
+
+	//消息广播的channel
+	Message chan string
 }
 
 //创建一个Server的接口
@@ -15,13 +23,51 @@ func NewServer (ip string, port int) *Server {
 	server := &Server{
 		Ip:	ip,
 		Port: port,
+		OnlineMap: make(map[string]*User),
+		Message: make(chan string),
 	}
 	return server
 }
 
+//广播消息的方法
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	
+	this.Message <- sendMsg
+}
+
+//监听广播消息channel的goroutine，一旦有消息就发送给全部的在线User
+func (this *Server) ListenMessage() {
+	for {
+		msg := <- this.Message
+
+		//将msg发送给全部的在线User
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
 func (this *Server) Handler(conn net.Conn) {
 	//...当前链接的业务
-	fmt.Println("链接建立成功")
+	// fmt.Println("链接建立成功")
+
+	user := NewUser(conn)
+
+	//用户上线,将用户加入到onlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+	
+	//广播当前用户上线消息
+	this.BroadCast(user, "已上线")
+
+	//当前handler阻塞
+	//里面没有任何 case，也就永远等不到可执行的分支
+	// 于是当前 goroutine 会 一直挂起，既不会退出，也不会让函数返回。
+	select {}
 
 }
 
@@ -35,6 +81,9 @@ func (this *Server) Start() {
 	}
 	//close listen socket
 	defer listener.Close()
+
+	//启动监听Message的goroutine
+	go this.ListenMessage()
 
 	for {
 		// accept
