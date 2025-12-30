af9b715eb9520eb0c97bca4a88669a0717c51364
wuxuanok <2031610355@qq.com>
2025-12-30 21:14:13 +0800

    在线用户查询

--- user.go
+++ user.go
@@ -50,10 +50,27 @@ func (this *User) Offline() {
 	this.server.BroadCast(this, "下线")
 }
 
+//给当前User对应的客户端发送消息
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg))
+}
+
 //用户处理消息的业务
 func (this *User) DoMessage(msg string) {
-	//将得到的消息进行广播
-	this.server.BroadCast(this, msg)
+	if msg == "who" {
+		//查询当前在线用户都有哪些
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.Unlock()
+
+	} else {
+		//将得到的消息进行广播
+		this.server.BroadCast(this, msg)
+	}
+
 }
 
 
