0ad2256f707b0078ed989a1eae1897b751d1b5b7
wuxuanok <2031610355@qq.com>
2025-12-31 23:54:33 +0800

    客户端实现-菜单显示

--- client/client.go
+++ client/client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 	//连接server
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
@@ -29,6 +31,46 @@ func NewClient(serverIp string, serverPort int) *Client {
 
 	//返回对象
 	return client
+}
+
+func (client *Client) menu() bool {
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>请输入合法范围内数字<<<<")
+		return false
+	}
+
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+		}
+
+		//根据不同的模式处理不同的业务
+		switch client.flag {
+		case 1:
+			//公聊模式
+			fmt.Println("公聊模式选择...")
+		case 2:
+			//私聊模式
+			fmt.Println("私聊模式选择...")
+		case 3:
+			//更新用户名
+			fmt.Println("更新用户名选择...")
+		}
+	}
 }
 
 var severIp string
@@ -54,5 +96,5 @@ func main() {
 	fmt.Println(">>>>>>连接服务器成功>>>>>>")
 
 	//启动客户端的业务
-	select {}
+	client.Run()
 }
